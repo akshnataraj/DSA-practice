e6cb6fe8d48306fd326f9a688b8fa5bcbd4967a7
akshnataraj <akshnat410@gmail.com>
2020-06-23 23:52:07 +0530

    Algorithm for Searching a rotated array

---
+++ searching_a_rotated_array.py
@@ -0,0 +1,14 @@
+'''
+Algorithm:
+Input arr[] = {3, 4, 5, 1, 2}
+Element to Search = 1
+  1) Find out pivot point and divide the array in two
+      sub-arrays. (pivot = 2) /*Index of 5*/
+  2) Now call binary search for one of the two sub-arrays.
+      (a) If element is greater than 0th element then
+             search in left array
+      (b) Else Search in right array
+          (1 will go in else as 1 < 0th element(3))
+  3) If element is found in selected sub-array then return index
+     Else return -1.
+ '''
