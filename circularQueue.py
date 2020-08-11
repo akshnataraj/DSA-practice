99efb7794985b9e16a98288c78ad36d4f4bc4a40
akshnataraj <akshnat410@gmail.com>
2020-08-11 07:41:44 +0530

    Circular queue

--- circularQueue.py
+++ circularQueue.py
@@ -4,64 +4,75 @@ class MyCircularQueue:
         """
         Initialize your data structure here. Set the size of the queue to be k.
         """
-        self.q = [None]*k
-        self.size = k
-        self.front = -1
-        self.rear = -1
-        
+        self._queue = [None] * k #the basic 
+        self._capacity = k 
+        self._len = 0 
+        #The first three as one group and the last
+        self._front = 0
+        #self._rear is not necessary, because rear = front + length -1
+
 
     def enQueue(self, value: int) -> bool:
         """
         Insert an element into the circular queue. Return true if the operation is successful.
         """
-        if (isFull()==True):
-            return False
-        self.q[self.rear] = value
-        self.rear = (self.rear + 1) % self.k
-        return True
-        
+        if self.isFull(): return False
+        avail = (self._front + self._len) % (self._capacity)
+        self._queue[avail] = value 
+        self._len += 1
+        return True 
 
+       
     def deQueue(self) -> bool:
         """
         Delete an element from the circular queue. Return true if the operation is successful.
         """
-        if (isEmpty()==True):
-            return False
-        self.q[self.front] = None
-        self.front = (self.front + 1) % self.k
+        if self.isEmpty():
+            return False 
+        self._queue[self._front] = None #overrode the current node 
+        self._front = (self._front+1) % self._capacity 
+        self._len -= 1
         return True
+
         
 
     def Front(self) -> int:
         """
         Get the front item from the queue.
         """
-        return self.q[self.front]
+        if not self.isEmpty():
+            return self._queue[self._front]
+        else:
+            return -1
+        
 
     def Rear(self) -> int:
         """
         Get the last item from the queue.
         """
-        return self.q[self.rear]
+        if not self.isEmpty():
+            _rear = (self._front + self._len - 1) % self._capacity
+            return self._queue[_rear]
+        else:
+            return -1
         
 
     def isEmpty(self) -> bool:
         """
         Checks whether the circular queue is empty or not.
         """
-        if(len(self.q)==)
-        
+        return self._len == 0 
         
 
     def isFull(self) -> bool:
         """
         Checks whether the circular queue is full or not.
         """
-        
+        return self._len == self._capacity   
 
 
 # Your MyCircularQueue object will be instantiated and called as such:
-obj = MyCircularQueue(k)
+# obj = MyCircularQueue(k)
 # param_1 = obj.enQueue(value)
 # param_2 = obj.deQueue()
 # param_3 = obj.Front()
